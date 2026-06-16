48e4dab5fb95125fb0493b779edcfa673f760531
Filip <filipsmolcec@gmail.com>
2026-06-17 00:33:53 +0200

    Fix wrong cstr length

--- src/base.c
+++ src/base.c
@@ -3,7 +3,7 @@
 
 bool sv_is_equal(StringView sv1, StringView sv2) {
   char cstr1[sv1.length + 1];
-  char cstr2[sv2.length + 2];
+  char cstr2[sv2.length + 1];
 
   strncpy(cstr1, sv1.start, sv1.length);
   strncpy(cstr2, sv2.start, sv2.length);
