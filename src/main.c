505d2095c0b28fdcf71072e64d00a65280e3da72
Filip <filipsmolcec@gmail.com>
2026-06-19 20:58:06 +0200

    Add filepath arguments to main

--- src/main.c
+++ src/main.c
@@ -1,10 +1,39 @@
 #include "base.h"
 #include "program.h"
+#include <stdio.h>
 
-i32 main(void) {
-  const char *sample_source = "1 + 52; { 10 + 2.5; { 1 + 10; } 5 + 1; }";
+void print_usage() {
+  printf(
+    "Usage:\n"
+    "  mel <filepath>\n"
+  );
+}
 
-  program_run(sample_source);
+char *read_entire_file(const char *filepath) {
+  FILE *f = fopen(filepath, "rb");
+  if (f == NULL) {
+    printf("Failed to open file\n");
+    return NULL;
+  }
+  fseek(f, 0, SEEK_END);
+  u64 fsize = ftell(f);
+  fseek(f, 0, SEEK_SET);
+
+  char *dest = malloc(fsize + 1);
+  fread(dest, fsize, sizeof(char), f);
+  fclose(f);
+
+  return dest;
+}
+
+i32 main(i32 argc, char **argv) {
+  if (argc <= 1) {
+    print_usage();
+    return 0;
+  }
+
+  char *source = read_entire_file(argv[1]);
+  program_run(source);
 
   return 0;
 }
