ffac4f61215f0bac8e3cca418c9a5452f33dc967
Filip <filipsmolcec@gmail.com>
2026-06-20 02:52:34 +0200

    Add native min and max functions

--- src/native.c
+++ src/native.c
@@ -1,4 +1,5 @@
 #include "native.h"
+#include "parser.h"
 
 Value native_print(Value *args, u32 count) {
   if (count < 1) {
@@ -12,7 +13,33 @@ Value native_print(Value *args, u32 count) {
   return NULL_VALUE;
 }
 
+Value native_min(Value *args, u32 count) {
+  if (count != 2) {
+    return NULL_VALUE;
+  }
+
+  if (args[0].kind != VAL_NUMBER || args[1].kind != VAL_NUMBER) {
+    return NULL_VALUE;
+  }
+
+  return args[0].as.number > args[1].as.number ? args[1] : args[0];
+}
+
+Value native_max(Value *args, u32 count) {
+  if (count != 2) {
+    return NULL_VALUE;
+  }
+
+  if (args[0].kind != VAL_NUMBER || args[1].kind != VAL_NUMBER) {
+    return NULL_VALUE;
+  }
+
+  return args[0].as.number > args[1].as.number ? args[0] : args[1];
+}
+
 NativeFn map_identifier_to_native_fn(StringView identifier) {
   if (sv_is_equal_to_cstr(identifier, "print")) return native_print;
+  else if (sv_is_equal_to_cstr(identifier, "min")) return native_min;
+  else if (sv_is_equal_to_cstr(identifier, "max")) return native_max;
   return NULL;
 }
